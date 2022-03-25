343e3c0326534fd1807be31a13c9299484314e7c
Yamil Ferrufino <yferrufino@piconsulting.com.ar>
2022-03-25 15:26:31 -0300

    agregue unidades_regex

--- codigos_pi/regex.py
+++ codigos_pi/regex.py
@@ -1,4 +1,6 @@
 import re
+from functools import reduce
+import pandas as pd
 def extraer():
     lista = []
     
@@ -9,11 +11,27 @@ def extraer():
             print (dato)
         
         
+def extraer_unidades (cadena):
     
-        
+    print (cadena)
+
+
+validar = lambda x: '0' if x == None else re.search ("[\d]{1,2}",x.group()).group()
+ 
+def extraer_unidades (df):
+    
+    lista = list (map(lambda x: validar (re.search("((x[\s]{0,1}[\d]{1})\s|([\d]{1}x[\d])|(pack x\s[\d]{1}\s)|(x pack [\d]{1})|(\s[\d]{1}\s[un]{1,2})|(\([\d]{2} unidades\)))",x)),df["DescripcionLargaDelArticulo"]))
+    df["cantidad"] = lista
+    
+    return df
+     
+cadena = "pack linea coca cola x500cc (12 unidades)"
 
 
 
 ######programa principal#####
-        
-extraer()
+#dato = re.search("\([\d]{1,2} unidades\)",cadena)
+data = pd.read_csv("unidades.csv")
+df =extraer_unidades(data)
+print (df.head(50))
+#extraer()
